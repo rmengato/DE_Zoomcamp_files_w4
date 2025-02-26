24a6c319ef979af17b4b26b88025f23f15b5ebce
Raphael Mengato <rmengato.da@gmail.com>
2025-02-26 23:36:18 +0000

    revenue corrections

--- models/core/fct_taxi_trips_quarterly_revenue.sql
+++ models/core/fct_taxi_trips_quarterly_revenue.sql
@@ -1,10 +1,12 @@
-with fct_trips_qtr_year as (
-    select *,
-    DATEPART('YEAR', pickup_datetime) AS YEAR,
-    DATEPART('QUARTER', pickup_datetime) as quarter
-    from {{ ref('fact_trips') }}
-)
-
-
-
-select * from fct_trips_qtr_year limit 10
+with
+    fct_trips_qtr_year as (
+        select
+            *,
+            extract(year from pickup_datetime) as year,
+            extract(quarter from pickup_datetime) as quarter
+        from {{ ref("fact_trips") }}
+    )
+select service_type, year, quarter, sum(total_amount) as sum_amount
+from fct_trips_qtr_year
+group by service_type, year, quarter
+order by service_type, sum_amount
